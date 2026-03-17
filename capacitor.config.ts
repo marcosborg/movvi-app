d0d7ef342d7e15d0984744147701c8752d1831b1
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2026-03-17 10:26:07 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.movvi',
+  appName: 'movvi',
+  webDir: 'dist'
+};
+
+export default config;
